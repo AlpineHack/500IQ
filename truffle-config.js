421560c8cfbff0b78b4341702107d2e23031b70b
Tavernier Martin <55973969+MartTave@users.noreply.github.com>
2019-10-05 10:14:22 +0200

    Add files via upload

--- truffle-config.js
+++ truffle-config.js
@@ -15,26 +15,16 @@ module.exports = {
         }
     },
     networks: {
-        _development: {
-            host: 'localhost',
-            port: 9545,
-            network_id: 4447,
-            gasPrice: 1,
-            gas: 4700000
+        develop: {
+            port: 8545
         },
-        local: {
-            host: 'localhost',
-            port: 8547,
-            network_id: '*',
-            gasPrice: 1,
-            gas: 6712390
-        },
+        /*
         ropsten: {
             provider: new HDWalletProvider("d0b1a82345eb5624494c984df1a942a53e1a9583a6376951ed539c28dcb2aad8", "https://ropsten.infura.io/"),
             network_id: 3,
             gasPrice: 10000000000,
             gas: 4712394
-        }
+        }*/
     },
     compilers: {
         solc: {
